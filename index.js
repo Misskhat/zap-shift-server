2c78fd5a608612b2f45e4472f9a6a4252e5c9566
Misskhat <misskhat.hossen@gmail.com>
2025-12-09 17:06:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const {MongoClient, ServerApiVersion} = require("mongodb");
+const port = process.env.PORT || 3000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+// mongodb connection uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5bt6oyo.mongodb.net/?appName=Cluster0`;
+
+app.get("/", (req, res) => {
+    res.send("zap shift server running!");
+});
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const db = client.db("zapShiftDB");
+        const parcelsCollection = db.collection("parcels");
+
+        // parcels api
+        app.get("/parcels", async (req, res) => {
+            const query = {};
+            const {email} = req.query;
+            if(query){
+                query.senderEmail = email
+            }
+            const cursor = parcelsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.post("/parcels", async (req, res) => {
+            const parcel = req.body;
+            const result = await parcelsCollection.insertOne(parcel);
+            res.send(result);
+        });
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ping: 1});
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
